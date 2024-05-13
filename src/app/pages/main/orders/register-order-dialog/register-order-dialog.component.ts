11998da67a356e04c38aa425ee4a92385674edb9
Cristian-Paz <cricripaz6@gmail.com>
2024-05-12 22:57:53 -0400

    Implement Register Order

--- src/app/pages/main/orders/register-order-dialog/register-order-dialog.component.ts
+++ src/app/pages/main/orders/register-order-dialog/register-order-dialog.component.ts
@@ -1,5 +1,14 @@
 import {Component, OnInit} from '@angular/core';
-import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {
+  FormArray,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  FormsModule,
+  NgForm,
+  ReactiveFormsModule,
+  Validators
+} from "@angular/forms";
 import {MatButtonModule} from "@angular/material/button";
 import {MatIconModule} from "@angular/material/icon";
 import {NgForOf, NgIf} from "@angular/common";
@@ -7,6 +16,7 @@ import {CustomerApiService} from "../../../../services/customer-api.service";
 import {Customer} from "../../../../models/customer.model";
 import {log} from "@angular-devkit/build-angular/src/builders/ssr-dev-server";
 import {map} from "rxjs/operators";
+import {ThisReceiver} from "@angular/compiler";
 
 @Component({
   selector: 'app-register-order-dialog',
@@ -23,42 +33,55 @@ import {map} from "rxjs/operators";
   styleUrl: './register-order-dialog.component.scss'
 })
 export class RegisterOrderDialogComponent implements OnInit{
-
-  products = [];
-  customers : any[] = [];
+  formOrder!: FormGroup;
+  customers: Customer[] = [];  // Assuming Customer is properly defined
 
   constructor(
-    private customerService : CustomerApiService
-  ) {
-  }
-
+    private customerService: CustomerApiService,
+    private fb: FormBuilder
+  ) {}
 
   ngOnInit(): void {
-    this.getCustomers()
-
+    this.initForm();
+    this.getCustomers();
   }
 
-
-  getCustomers(){
-
-    this.customerService.fetchCustomers()
-      .subscribe(data => {
-        // Assuming 'data.customers' is the correct path if 'data' and 'data.customers' are valid
-        this.customers = data && data.customers ? data.customers : [];
-
-        this.customers.sort((a, b) => (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0));
-      });
-
-  }
-  onSubmit(DataUserForm: any) {
-
+  private initForm(): void {
+    this.formOrder = this.fb.group({
+      customer: ['', Validators.required],
+      products: this.fb.array([this.createProduct()])
+    });
   }
 
-  removeProduct(i: number) {
-
+  get products(): FormArray {
+    return this.formOrder.get('products') as FormArray;
   }
 
-  addProduct() {
-
+  private createProduct(): FormGroup {
+    return this.fb.group({
+      type: ['', Validators.required],
+      color: ['', Validators.required]
+    });
   }
+
+  getCustomers(): void {
+    this.customerService.fetchCustomers().subscribe({
+      next: (data: any[]) => this.customers = data,
+      error: (err: any) => console.error('Failed to fetch customers', err)
+    });
+  }
+
+  onSubmit(): void {
+    console.log(this.formOrder.value);
+    // Handle form submission
+  }
+
+  addProduct(): void {
+    this.products.push(this.createProduct());
+  }
+
+  removeProduct(index: number): void {
+    this.products.removeAt(index);
+  }
+
 }

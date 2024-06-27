d36be3fe99bb6b06de0769738a779f77b23090b7
Cristian Paz <cricripaz@MacBook-Pro-de-Cristian.local>
2024-06-27 12:05:14 -0500

    add test for inventory add weight

--- src/app/pages/main/inventory-table-tintes/add-weight-inventory/add-weight-inventory.component.spec.ts
+++ src/app/pages/main/inventory-table-tintes/add-weight-inventory/add-weight-inventory.component.spec.ts
@@ -1,17 +1,53 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ReactiveFormsModule, FormBuilder } from '@angular/forms';
+import { MAT_DIALOG_DATA, MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { AddWeightInventoryComponent } from './add-weight-inventory.component';
+import { InventoryApiService } from '../../../../services/inventory-api.service';
+import { ToastrService } from 'ngx-toastr';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { of, throwError } from 'rxjs';
 
 describe('AddWeightInventoryComponent', () => {
   let component: AddWeightInventoryComponent;
   let fixture: ComponentFixture<AddWeightInventoryComponent>;
+  let mockInventoryApiService: any;
+  let mockMatDialog: any;
+  let mockToastrService: any;
+  let mockDialogData: any;
 
   beforeEach(async () => {
+    mockInventoryApiService = {
+      addQuantityWeight: jest.fn()
+    };
+
+    mockMatDialog = {
+      closeAll: jest.fn()
+    };
+
+    mockToastrService = {
+      success: jest.fn(),
+      error: jest.fn()
+    };
+
+    mockDialogData = {
+      dyeInventory_id: 1041,
+      name: "ACID BLACK"
+    };
+
     await TestBed.configureTestingModule({
-      imports: [AddWeightInventoryComponent]
-    })
-    .compileComponents();
-    
+      imports: [ReactiveFormsModule, MatDialogModule, AddWeightInventoryComponent],
+      providers: [
+        FormBuilder,
+        { provide: InventoryApiService, useValue: mockInventoryApiService },
+        { provide: MatDialog, useValue: mockMatDialog },
+        { provide: ToastrService, useValue: mockToastrService },
+        { provide: MAT_DIALOG_DATA, useValue: mockDialogData }
+      ],
+      schemas: [NO_ERRORS_SCHEMA],
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
     fixture = TestBed.createComponent(AddWeightInventoryComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -19,5 +55,51 @@ describe('AddWeightInventoryComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should initialize the form with dialog data', () => {
+    component.ngOnInit();
+    expect(component.addWeightForm).toBeDefined();
+    expect(component.addWeightForm.controls['id'].value).toBe(mockDialogData.dyeInventory_id);
+    expect(component.addWeightForm.controls['type'].value).toBe('');
+    expect(component.addWeightForm.controls['weigth'].value).toBe('');
+  });
+
+  it('should display error if form is invalid on submit', () => {
+    component.addWeigth();
+    expect(mockToastrService.error).toHaveBeenCalledWith('Por favor llena Todos los campos');
+  });
+
+  it('should submit form and display success message', () => {
+    component.ngOnInit();
+    component.addWeightForm.setValue({
+      id: mockDialogData.dyeInventory_id,
+      type: 'gr',
+      weigth: 100
+    });
+
+    mockInventoryApiService.addQuantityWeight.mockReturnValue(of({ message: 'Weight added successfully' }));
+
+    component.addWeigth();
+
+    expect(mockInventoryApiService.addQuantityWeight).toHaveBeenCalledWith(component.addWeightForm.value);
+    expect(mockToastrService.success).toHaveBeenCalledWith(`Exito ,Cantidad Agregada : 100`);
+    expect(mockMatDialog.closeAll).toHaveBeenCalled();
+  });
+
+  it('should display error message on submit failure', () => {
+    component.ngOnInit();
+    component.addWeightForm.setValue({
+      id: mockDialogData.dyeInventory_id,
+      type: 'gr',
+      weigth: 100
+    });
+
+    mockInventoryApiService.addQuantityWeight.mockReturnValue(throwError('Error'));
+
+    component.addWeigth();
+
+    expect(mockInventoryApiService.addQuantityWeight).toHaveBeenCalledWith(component.addWeightForm.value);
+    expect(mockToastrService.error).toHaveBeenCalledWith('Por favor llena Todos los campos');
   });
 });
--- src/app/pages/main/inventory-table-tintes/add-weight-inventory/add-weight-inventory.component.ts
+++ src/app/pages/main/inventory-table-tintes/add-weight-inventory/add-weight-inventory.component.ts
@@ -1,8 +1,8 @@
-import {Component, Inject, OnInit} from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import {MAT_DIALOG_DATA, MatDialog, MatDialogModule} from "@angular/material/dialog";
-import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule} from "@angular/forms";
-import {InventoryApiService} from "../../../../services/inventory-api.service";
-import {ToastrService} from "ngx-toastr";
+import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
+import { InventoryApiService } from "../../../../services/inventory-api.service";
+import { ToastrService } from "ngx-toastr";
 
 @Component({
   selector: 'app-add-weight-inventory',
@@ -13,57 +13,41 @@ import {ToastrService} from "ngx-toastr";
     FormsModule
   ],
   templateUrl: './add-weight-inventory.component.html',
-  styleUrl: './add-weight-inventory.component.scss'
+  styleUrls: ['./add-weight-inventory.component.scss']
 })
-export class AddWeightInventoryComponent implements OnInit{
+export class AddWeightInventoryComponent implements OnInit {
   addWeightForm!: FormGroup;
 
   constructor(
-    @Inject(MAT_DIALOG_DATA) public data : any ,
-    private fb : FormBuilder,
-    private matdialog : MatDialog,
-    private inventoryservice : InventoryApiService,
-    private toastr : ToastrService
-  ) {
-  }
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private fb: FormBuilder,
+    private matdialog: MatDialog,
+    private inventoryService: InventoryApiService,
+    private toastr: ToastrService
+  ) {}
 
   ngOnInit(): void {
+    this.initializeForm();
+  }
 
-    this.initializeForm()
-    }
-
-
-
-    private initializeForm (){
+  private initializeForm() {
     this.addWeightForm = this.fb.group({
-      id:[this.data.dyeInventory_id],
-      type:[''],
-      weigth:['']
-    })
-    }
+      id: [this.data.dyeInventory_id],
+      type: ['', Validators.required],
+      weigth: ['', Validators.required]
+    });
+  }
 
   addWeigth() {
-
-    if (this.addWeightForm.valid){
-
-      console.log(this.addWeightForm.value)
-
-      this.inventoryservice.addQuantityWeight(this.addWeightForm.value).subscribe( () => {
-
-        this.matdialog.closeAll()
-
-        this.toastr.success(`Exito ,Cantidad Agregada : ${this.addWeightForm.value.weigth}`)
-
-        //TODO update values weight parent
-      })
-
-
-    }else {
-
-      this.toastr.error('Por favor llena Todos los campos')
-
+    if (this.addWeightForm.valid) {
+      this.inventoryService.addQuantityWeight(this.addWeightForm.value).subscribe(() => {
+        this.matdialog.closeAll();
+        this.toastr.success(`Exito ,Cantidad Agregada : ${this.addWeightForm.value.weigth}`);
+      }, () => {
+        this.toastr.error('Error al agregar la cantidad.');
+      });
+    } else {
+      this.toastr.error('Por favor llena Todos los campos');
     }
-
-
   }
 }
